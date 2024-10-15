e3a03086e92913d894e4451027872ec1fec95399
ventell virgo <lennyasme@gmail.com>
2024-10-15 15:30:28 -0400

    updated API.tsx to debug error for render

--- src/api/API.tsx
+++ src/api/API.tsx
@@ -1,4 +1,4 @@
-const searchGithub = async (search: string) => {
+const searchGithub = async (_search: string) => {
   try {
     const start = Math.floor(Math.random() * 100000000) + 1;
     // console.log(import.meta.env);
